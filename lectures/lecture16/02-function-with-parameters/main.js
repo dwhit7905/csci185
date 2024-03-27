e68a806442fdc630b16d39185cdc0ddadfcaaa64
Destiny <destinyhardtw@gmail.com>
2024-03-27 02:55:55 -0400

    project

---
+++ lectures/lecture16/02-function-with-parameters/main.js
@@ -0,0 +1,16 @@
+function changeColor(color) {
+    const bodyEl = document.querySelector("body");
+    bodyEl.style.backgroundColor = color
+}
+
+// function makeRed(){
+//     document.querySelector("body").style.backgroundColor = "#B23";
+// }
+
+// function makeBlue() {
+//     document.querySelector("body").style.backgroundColor = "#2AF";
+// }
+
+// function makeGreen(){
+//     document.querySelector("body").style.backgroundColor = "green"
+// }
